c918bfe88200cbdfeef44f519f1ddd809a64e42c
ansarav <ansaravi@ucsd.edu>
2022-02-04 13:17:10 -0800

    lib + code that fails :(

---
+++ SkillDemoTest.java
@@ -0,0 +1,11 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    
+    @Test
+    public void Subtract(){
+        assertEquals(4, SkillDemo.Subtract(4, 1)); // purposelly making it fail
+    }
+}
